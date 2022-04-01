aef5d678fdb90c001c361a5411d7fd478d6ad8dc
unknown <Administrator@MEDICAURUGUAYA.LOCAL>
2022-04-01 17:10:31 -0300

    update 01-04/3

---
+++ src/main/java/com/services/api/ffmanager/controller/FFManagerController.java
@@ -0,0 +1,14 @@
+package com.services.api.ffmanager.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+@RequestMapping(value = "/ffmanager/services", produces = MediaType.APPLICATION_JSON_VALUE)
+public class FFManagerController {
+
+}
